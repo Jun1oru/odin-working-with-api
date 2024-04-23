3e772074e84b01e7fc75f5ffefde562762a16f6d
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2024-04-23 23:24:41 +0300

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+};
